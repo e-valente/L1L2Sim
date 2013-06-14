d617a9de071ac3c197ec8249fe8a776185478bb1
emanuelvalente <emanuelvalente@gmail.com>
2013-06-14 01:20:32 -0300

    int to char em big endian

--- memory_manager.c
+++ memory_manager.c
@@ -11,9 +11,35 @@
  * */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include "memory_manager.h"
 
 int memory_access(int addr, int *data, int type) {
   /* Seu codigo comeca aqui :) */
+
+	unsigned char bytes[4];
+	int i;
+	int num = *data;
+
+	if(type == 0)
+		printf("Leitura....\n");
+	if(type ==1)
+	{
+		printf("Escrita..\n");
+
+		for(i = 0; i < 4; i++)
+		{
+			bytes[i] = num & 0xFF;
+			printf("byte %d = %d\n", i, bytes[i]);
+			num >>= 8;
+
+		}
+		memory[0] = bytes[3];
+		memory[1] = bytes[2];
+		memory[2] = bytes[1];
+		memory[3] = bytes[0];
+
+	}
+
   return -1;
 }
